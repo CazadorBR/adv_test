d9892cadbf9bced2ebd0715ab56d357d505c3dff
Ben Romdhane Fedi <fedi.benromdhane@gmail.com>
2024-10-08 21:33:11 +0100

    Add missing NEXTAUTH-SECRET

--- src/app/lib/authOptions.ts
+++ src/app/lib/authOptions.ts
@@ -10,6 +10,8 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,  
+
   callbacks: {
     async signIn({ user }) {
       try {
